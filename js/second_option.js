9c657c0c8d0db3a4523d9f569afffa6069c9c4fe
aishatrue <140340401+aishatrue@users.noreply.github.com>
2023-11-20 23:28:42 +0900

    1 day 1 commit

--- js/second_option.js
+++ js/second_option.js
@@ -2,9 +2,21 @@ const first = document.getElementById("first");
 const second = document.getElementById("second");
 
 function saveResult(event) {
+  // event.preventDefault();
   const value = event.target.dataset.value;
+  console.log(value, "벨류학인");
+  const currentVal = window.localStorage.getItem("second_option");
+  console.log(currentVal);
+  let valArray = [];
 
-  window.localStorage.setItem("first_option", value);
+  if (currentVal) {
+    valArray = JSON.parse(currentVal);
+    valArray.push(value);
+    window.localStorage.setItem("second_option", JSON.stringify(valArray));
+  } else {
+    valArray.push(value);
+    window.localStorage.setItem("second_option", JSON.stringify(valArray));
+  }
 }
 
 first.addEventListener("click", saveResult);
